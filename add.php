898a181f5538bd8f88c15e0235ec4688f8559f08
lizaedwar <edwar2016pi21la@gmail.com>
2023-07-11 18:09:51 -0600

    Uff, update number

--- add.php
+++ add.php
@@ -4,82 +4,82 @@
 
   $error = null;
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-      if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
-        $error = "Please fill all the fields.";
-      } else if (strlen($_POST["phone_number"]) < 10){
-        $error = "Phone number must be at least 10 characters.";
-      } else {
-        $name = $_POST["name"];
-        $phoneNumber = $_POST["phone_number"];
-    
-        $statement = $connection->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name,:phoneNumber)");
-        $statement->bindParam(":name", $_POST["$name"]);
-        $statement->bindParam(":phone_number", $_POST["$phone_number"]);
-        $statement->execute();
-    
-        header("Location: index.php");
-      }
-   
-  } ?>
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (empty($_POST["name"]) || empty($_POST["phone_number"])) {
+      $error = "Please fill all the fields.";
+    } else if (strlen($_POST["phone_number"]) < 9) {
+      $error = "Phone number must be at least 9 characters.";
+    } else {
+      $name = $_POST["name"];
+      $phoneNumber = $_POST["phone_number"];
+
+      $statement = $connection->prepare("INSERT INTO contacts (name, phone_number) VALUES (:name, :phone_number)");
+      $statement->bindParam(":name", $_POST["name"]);
+      $statement->bindParam(":phone_number", $_POST["phone_number"]);
+      $statement->execute();
+
+      header("Location: index.php");
+    }
+  }
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<!-- bootstrap control + k + c para comentar -->
-  <link
-  rel="stylesheet" 
-  href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.3.0/darkly/bootstrap.min.css"
-  integrity="sha512-3xynESL0QF3ERUl9se1VJk043nWT+UzWJveifBw7kLtC226vyGINZFtmyK015F83KBSNW+67alYSY2cCj1LHOQ=="
-  crossorigin="anonymous"
-  referrerpolicy="no-referrer"
-  />
 
+  <!-- Bootstrap -->
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/bootswatch/5.1.3/darkly/bootstrap.min.css"
+    integrity="sha512-ZdxIsDOtKj2Xmr/av3D/uo1g15yxNFjkhrcfLooZV5fW0TT7aF7Z3wY1LOA16h0VgFLwteg14lWqlYUQK3to/w=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
   <script
-  defer
-  src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
-  crossorigin="anonymous"
+    defer
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+    crossorigin="anonymous"
   ></script>
 
-  <link rel="stylesheet" href="./static/css/index.css">
+  <!-- Static Content -->
+  <link rel="stylesheet" href="./static/css/index.css" />
 
   <title>Contacts App</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand font-weight-bold" href="#">
-          <img class="mr-2" src="./static/img/logo.png" />
-          ContactsApp
-        </a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="./index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="./add.php">Add Contact</a>
-            </li>
-          </ul>
-        </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand font-weight-bold" href="#">
+        <img class="mr-2" src="./static/img/logo.png" />
+        ContactsApp
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./add.php">Add Contact</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-
-
+    </div>
+  </nav>
 
   <main>
     <div class="container pt-5">
@@ -90,10 +90,10 @@
             <div class="card-body">
               <?php if ($error): ?>
                 <p class="text-danger">
-                <?= $error ?>
+                  <?= $error ?>
                 </p>
-                <?php endif ?>
-              <form method="post" action="add.php">
+              <?php endif ?>
+              <form method="POST" action="add.php">
                 <div class="mb-3 row">
                   <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
     
@@ -109,7 +109,6 @@
                     <input id="phone_number" type="tel" class="form-control" name="phone_number" autocomplete="phone_number" autofocus>
                   </div>
                 </div>
-                
     
                 <div class="mb-3 row">
                   <div class="col-md-6 offset-md-4">
@@ -123,6 +122,5 @@
       </div>
     </div>
   </main>
-
 </body>
 </html>
