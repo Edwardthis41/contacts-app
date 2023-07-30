29ec98401815bb23f29ed1bbffdfd8853384777b
lizaedwar <edwar2016pi21la@gmail.com>
2023-07-30 09:53:22 -0600

    Send 403 if contact does not belong to user

--- delete.php
+++ delete.php
@@ -11,13 +11,21 @@ if (!isset($_SESSION["user"])) {
 
 $id = $_GET["id"];
 
-$statement = $connection->prepare("SELECT * FROM contacts WHERE id = :id");
+$statement = $connection->prepare("SELECT * FROM contacts WHERE id = :id LIMIT 1");
 $statement->bindParam(":id", $id);
 $statement->execute();
 
 if ($statement->rowCount() == 0) {
   http_response_code(404);
   echo("HTTP 404 NOT FOUND");
+  return;
+}
+
+$contact = $statement->fetch(PDO::FETCH_ASSOC);
+
+if ($contact["user_id"] !== $_SESSION["user"]["id"]) {
+  http_response_code(403);
+  echo("HTTP 403 UNAUTHORIZED");
   return;
 }
 
