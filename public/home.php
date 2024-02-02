8cfc117b29e7d30d5fa3d02ae10d0c4eceea9b88
lizaedwar <edwar2016pi21la@gmail.com>
2024-02-02 17:16:01 -0600

    Agregamos las funcion para publicidad

--- public/home.php
+++ public/home.php
@@ -15,31 +15,43 @@ $contacts = $connection->query("SELECT * FROM contacts WHERE user_id = {$_SESSIO
 
 <?php require "partials/header.php" ?>
 
-      <div class="container pt-4 p-3">
-        <div class="row">
-          
-          <?php if ($contacts->rowCount() == 0): ?>
-            <div class="col-md-4 mx-auto">
-              <div class="card card-body text-center">
-                <p>No Existe Articulos</p>
-                <a href="add.php">Añadir Uno!</a>
-              </div>
-            </div>
-            <?php endif ?>
-          <?php foreach ($contacts as $contact): ?>
-            <div class="col-md-4 mb-3">
-              <div class="card text-center">
-                <div class="card-body">
-                  <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
-                  <p class="m-2"><?= $contact["phone_number"] ?></p>
-                  <a href="edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Editar Articulo</a>
-                  <a href="delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Eliminar</a>
-                </div>
-              </div>
-            </div>
-          <?php endforeach ?>
+<div class="container pt-4 p-3">
+  <div class="row">
 
+    <!-- Principal con bucle foreach a la izquierda -->
+    <div class="col-md-8 mb-3">
+      <?php if ($contacts->rowCount() == 0): ?>
+        <div class="card card-body text-center">
+          <p>No Existe Articulos</p>
+          <a href="add.php">Añadir Uno!</a>
+        </div>
+      <?php endif ?>
+
+      <?php foreach ($contacts as $contact): ?>
+        <div class="card text-center mb-3">
+          <div class="card-body">
+            <h3 class="card-title text-capitalize"><?= $contact["name"] ?></h3>
+            <p class="m-2"><?= $contact["phone_number"] ?></p>
+            <a href="edit.php?id=<?= $contact["id"] ?>" class="btn btn-secondary mb-2">Editar Articulo</a>
+            <a href="delete.php?id=<?= $contact["id"] ?>" class="btn btn-danger mb-2">Eliminar</a>
+            <a href="#" class="btn btn-warning mb-2">Publicar</a>
+          </div>
+        </div>
+      <?php endforeach ?>
+    </div>
+
+    <!-- Nueva tarjeta a la derecha en una columna de 4 -->
+    <div class="col-md-4 mb-3">
+      <div class="card text-center">
+        <div class="card-body">
+          <!-- Contenido de la nueva tarjeta -->
+          <h3 class="card-title">Nueva Tarjeta</h3>
         </div>
       </div>
+    </div>
+
+  </div>
+</div>
+
 
 <?php require "partials/footer.php" ?>
--- public/partials/footer.php
+++ public/partials/footer.php
@@ -1,3 +1,286 @@
     </main>
-  </body>
+<!-- footer de imagenes Inicio -->
+
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+
+  <footer class="text-center text-white" style="background-color: #caced1;">
+    <!-- Grid container -->
+    <div class="container p-4">
+      <!-- Section: Images -->
+      <section class="">
+        <div class="row">
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/113.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/111.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/112.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/114.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/115.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+          <div class="col-lg-2 col-md-12 mb-4 mb-md-0">
+            <div
+                 class="bg-image hover-overlay ripple shadow-1-strong rounded"
+                 data-ripple-color="light"
+                 >
+              <img
+                   src="https://mdbootstrap.com/img/new/fluid/city/116.jpg"
+                   class="w-100"
+                   />
+              <a href="#!">
+                <div
+                     class="mask"
+                     style="background-color: rgba(251, 251, 251, 0.2);"
+                     ></div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Section: Images -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+
+</div>
+<!-- End of .container -->
+
+
+    <!-- footer de imagens Final -->
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container my-5">
+  <!-- Footer -->
+  <footer
+          class="text-center text-lg-start text-white"
+          style="background-color: #45526e"
+          >
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: Links -->
+      <section class="">
+        <!--Grid row-->
+        <div class="row">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+            LEY ORGÁNICA DE COMUNICACIÓN
+            </h6>
+            <p>
+            SEGÚN EL ART. 60 DE LA LEY ORGÁNICA DE COMUNICACIÓN, LOS CONTENIDOS SE 
+            IDENTIFICAN Y CLASIFICAN EN: (I), INFORMATIVOS; (O), DE OPINIÓN; (F),
+            FORMATIVOS/EDUCATIVOS/CULTURALES; (E), ENTRETENIMIENTO; Y (D), DEPORTIVOS.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+            <p>
+              <a class="text-white">MDBootstrap</a>
+            </p>
+            <p>
+              <a class="text-white">MDWordPress</a>
+            </p>
+            <p>
+              <a class="text-white">BrandFlow</a>
+            </p>
+            <p>
+              <a class="text-white">Bootstrap Angular</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">
+              Useful links
+            </h6>
+            <p>
+              <a class="text-white">Your Account</a>
+            </p>
+            <p>
+              <a class="text-white">Become an Affiliate</a>
+            </p>
+            <p>
+              <a class="text-white">Shipping Rates</a>
+            </p>
+            <p>
+              <a class="text-white">Help</a>
+            </p>
+          </div>
+
+          <!-- Grid column -->
+          <hr class="w-100 clearfix d-md-none" />
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+            <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+            <p><i class="fas fa-envelope mr-3"></i> info@gmail.com</p>
+            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!--Grid row-->
+      </section>
+      <!-- Section: Links -->
+
+      <hr class="my-3">
+
+      <!-- Section: Copyright -->
+      <section class="p-3 pt-0">
+        <div class="row d-flex align-items-center">
+          <!-- Grid column -->
+          <div class="col-md-7 col-lg-8 text-center text-md-start">
+            <!-- Copyright -->
+            <div class="p-3">
+              © 2020 Copyright:
+              <a class="text-white" href="https://mdbootstrap.com/"
+                 >MDBootstrap.com</a
+                >
+            </div>
+            <!-- Copyright -->
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-5 col-lg-4 ml-lg-0 text-center text-md-end">
+            <!-- Facebook -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="fab fa-facebook-f"></i
+              ></a>
+
+            <!-- Twitter -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="fab fa-twitter"></i
+              ></a>
+
+            <!-- Google -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="fab fa-google"></i
+              ></a>
+
+            <!-- Instagram -->
+            <a
+               class="btn btn-outline-light btn-floating m-1"
+               class="text-white"
+               role="button"
+               ><i class="fab fa-instagram"></i
+              ></a>
+          </div>
+          <!-- Grid column -->
+        </div>
+      </section>
+      <!-- Section: Copyright -->
+    </div>
+    <!-- Grid container -->
+  </footer>
+  <!-- Footer -->
+</div>
+<!-- End of .container -->
+
+
+</body>
+
 </html>
